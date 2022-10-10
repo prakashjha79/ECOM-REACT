c8a90a7ab7751991089c4de8b24c46078c5e247d
prakash jha <prakashjhakk.pj@gmail.com>
2022-10-10 17:24:21 +0530

    Add background image

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
-// import './App.css';
-import './categories.scss'
+
+import "./categories.scss";
 
 function App() {
+    
+  const categories = [
+       
+    {
+      title: "Hats",
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
 
-  const categories=[
     {
-      title:"Hats"
+      title: "Jackets",
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
+
     {
-      title:"Jackets"
+      title: "Sneakers",
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
+
     {
-      title:"Sneakers"
+      title: "Womens",
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
+
     {
-      title:"Mens"
-    },
-    {
-      title:"Womens"
-    },
+      title: "Mens",
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    }
 
   ]
 
 
+
+
+
+
   return (
     <div className="App">
-     <div className='categories'>
+         
+           <div className="categories">
+              
+           {categories.map( ({title, imageUrl})=>(
+            <div className="category-container"
+             style={{backgroundImage: `url(${imageUrl})`}}
+            > 
+                    <div className="category-body">
+                          <h2>{title}</h2>
+                          <p>Shop Now</p>
+                     </div>
+                </div>
+           ))
+ }
 
-        {categories.map((category)=>(
-          <div className='category-container'>
-          {/* <img/> */}
-          <div className='category-body'>
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
 
-        </div>
-        ))
-        }
+
+                
+
+           </div>
         
-        
-     </div>
-
-
-
     </div>
   );
 }
