31650910eadcc2dcb1b6fc8470f454caea0f80dc
prakash jha <prakashjhakk.pj@gmail.com>
2022-10-10 16:51:14 +0530

    Add category component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 // import './App.css';
+import './categories.scss'
 
 function App() {
 
-  
+  const categories=[
+    {
+      title:"Hats"
+    },
+    {
+      title:"Jackets"
+    },
+    {
+      title:"Sneakers"
+    },
+    {
+      title:"Mens"
+    },
+    {
+      title:"Womens"
+    },
+
+  ]
 
 
   return (
     <div className="App">
-     <h2> Hello </h2>
+     <div className='categories'>
+
+        {categories.map((category)=>(
+          <div className='category-container'>
+          {/* <img/> */}
+          <div className='category-body'>
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+
+        </div>
+        ))
+        }
+        
+        
+     </div>
 
 
 
